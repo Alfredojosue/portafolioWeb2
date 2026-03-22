012f64201fdf1c361869fd1fe2162f26fc1708d7
Josue Ramirez <alfredojosueramirezchavez@gmail.com>
2026-03-21 22:19:04 -0600

    Insercion de los primeros apartados

--- index.php
+++ index.php
@@ -1,13 +1,109 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Portafolio Web</title>
     <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
+
 <body>
-    
+    <header>
+        <nav id="navbar">
+            <div class="logo">Josue<span> Ram&iacute;rez</span>
+                <ul class="nav-links">
+                    <li> <a href="#inicio"></a>Inicio</li>
+                    <li> <a href="#experiencia"></a>Experiencia</li>
+                    <li> <a href="#proyectos"></a>Proyectos</li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section id="inicio" class="section">
+            <div class="section-contec">
+                <div class="section-text">
+                    <h1>Josue Ram&iacute;rez</h1>
+                    <h2>Ing. sistemas computacionales</h2>
+                    <p>Soy ingeniero en sistemas computacionales con especialidad de desarrollo web y moviles.
+                        Cuento con gran capacidad aprendizaje, motivaci&oacute;n y entusiasmo por sobrellevar nuevos
+                        retos.</p>
+                    <div class="social-links">
+                        <a href="" target="_blank"><i class="bi bi-instagram"></i></a>
+                        <!-- <a href="" target="_blank"><i class="bi bi-whatsapp"></i></a> -->
+                        <a href="" target="_blank"><i class="bi bi-instagram"></i></a>
+                    </div>
+                </div>
+                <div class="section-image">
+                    <img src="./img/perfil.jpeg" alt="imagen de perfil">
+                </div>
+            </div>
+        </section>
+
+        <section id="experiencia" class="experiencia">
+            <h2 class="sectiion-titulo">Experiencia Laboral</h2>
+            <div class="timeline">
+                <div class="timeline-item">
+                    <div class="timeline-content">
+                        <span class="date"> 01 de julio 2025 - 31 diciembre 2025</span>
+                        <h3>Soporte tecnico Administrativo</h3>
+                        <h4>Secretaria de salud (CAPACITS)</h4>
+                        <p>Soporte tecnico a equipos de computo, ademas de generar
+                            reportes en formatos de excel proporcionado de
+                            un conjunto de datos de la instituci&oacute;n, adem&aacute;s
+                            manejo de inventario de farmacia y caputara de informaci&oacute;n
+                            en plataformas institucionales.
+                        </p>
+                        <ul class="Skills">
+                            <li>Excel</li>
+                            <li>Soporte de software</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="timeline-item">
+                    <div class="timeline-content">
+                        <span class="date"> 03 de octubre 2024 - 13 de marzo 2025</span>
+                        <h3>Ingeniero en infrestructura</h3>
+                        <h4>Salud Digna</h4>
+                        <p>Manejo de redes, cableado estructurado, ponchado de nodos y peinado del site, configuracion
+                            de equipos tecnologicos como switch, administraci&oacute;n de inventarios, requisiciones y
+                            destrucciones
+                            de equipos tecnologicos, configuracion de impresoras termicas, soporte tecnico a equipos de
+                            computo e impresoras, mantenimiento
+                            de camaras de seguridad CCTV.
+                        </p>
+                        <ul class="Skills">
+                            <li>cableado estructurado</li>
+                            <li>soporte tecnico</li>
+                            <li>Instalacion de camaras y mantenimiento</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="timeline-item">
+                    <div class="timeline-content">
+                        <span class="date">agosto 2023 - enero 2024</span>
+                        <h3>Desarrollador web Full-stack</h3>
+                        <h4>Centro de investigaci&oacute;n en matematicas</h4>
+                        <p>Desarrollo web informarmativo y de demostraci&oacute;n
+                        del funcionamiento de un sistema de inteligencia artificial
+                        con aplicacion al turismo. Utilizando framework como flask empleando modelo vista controlador, asi como
+                        lenguajes de programaci&oacute;n en python, html, css, javaScript etc.
+                        </p>
+                        <ul class="Skills">
+                            <li>flask</li>
+                            <li>python (Lenguaje de prosesamiento natural)</li>
+                            <li>html</li>
+                            <li>css</li>
+                            <li>javaScript</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
+
 </html>
